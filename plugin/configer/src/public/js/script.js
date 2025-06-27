cab6919c35411de931491a34da5b83b537aac90e
mdvtkr <mdvtkr@gmail.com>
2025-06-27 20:18:48 +0900

    tmp

--- plugin/configer/src/public/js/script.js
+++ plugin/configer/src/public/js/script.js
@@ -1,7 +1,7 @@
 const global = { 'filename': '' }
 
 async function fetchFiles() {
-    const res = await fetch('/api/files');
+    const res = await fetch('/api/configs');
     const files = await res.json();
     const filesDiv = document.getElementById('files');
     filesDiv.innerHTML = '<strong>Select a file:</strong><br>';
@@ -17,9 +17,12 @@ async function fetchFiles() {
 async function loadFile(filename) {
     global.filename = filename
 
-    const res = await fetch(`/api/file/${filename}`);
+    let res = await fetch(`/api/config/${filename}`);
     const data = await res.json();
-    renderEditor(data, filename);
+
+    res = await fetch(`api/content/${filename}/pending-count`)
+
+    renderEditor(data, filename, res.json.pendingCount);
 }
 
 // function renderObject(data, form, filename) {
@@ -57,7 +60,7 @@ async function loadFile(filename) {
 //             }
 
 //             // Save immediately
-//             fetch(`/api/file/${filename}`, {
+//             fetch(`/api/config/${filename}`, {
 //                 method: 'POST',
 //                 headers: { 'Content-Type': 'application/json' },
 //                 body: JSON.stringify(updatedData),
@@ -290,7 +293,7 @@ function clearChildrent(parent) {
     }
 }
 
-function renderEditor(data, filename) {
+function renderEditor(data, filename, pendingCount) {
     console.log('renderEditor')
     const editor = document.getElementById('editor');
     clearChildrent(editor)
@@ -301,7 +304,7 @@ function renderEditor(data, filename) {
     editor.appendChild(saveButton)
     saveButton.textContent = '파일에 저장'
     saveButton.onclick = () => {
-        fetch(`/api/file/${filename}`, {
+        fetch(`/api/config/${filename}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(getJsonFromHtml()),
@@ -317,6 +320,18 @@ function renderEditor(data, filename) {
     showAllButton.textContent = '전체보기'
     showAllButton.onclick = () => {
         showModalJson()
+    }
+
+    if (filename.includes('ddotty')) {
+        const uploadDiv = document.createElement('div')
+        const uploadButton = document.createElement('button')
+        uploadButton.textContent = '업로드'
+        uploadButton.onclick = () => {
+
+        }
+
+        uploadDiv.appendChild(uploadButton)
+        editor.appendChild(uploadDiv)
     }
 
     let list = document.createElement('ul');
@@ -341,7 +356,7 @@ function renderEditor(data, filename) {
 fetchFiles();
 
 function getReferenceByJsonPath(obj, path) {
-    if(path.length == 0 || path === '$')
+    if (path.length == 0 || path === '$')
         return { parent: null, key: null, target: obj }
 
     // Remove '$' and split on dots, handling array indexes
@@ -402,7 +417,7 @@ function showModalJson() {
     input.value = JSON.stringify(getJsonFromHtml(), null, 2)
     const saveButton = document.getElementById("btnSaveModal")
     saveButton.onclick = () => {
-        try { 
+        try {
             renderEditor(JSON.parse(input.value), global.filename)
             closeModal()
         } catch (err) {
--- plugin/configer/src/routes/api.js
+++ plugin/configer/src/routes/api.js
@@ -1,23 +1,66 @@
-const router = require('express').Router()
-const fs = require('fs')
-const path = require('path')
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const os = require('os');
 
 function expandHome(filepath) {
-  if (filepath.startsWith('~/')) {
-    return path.join(os.homedir(), filepath.slice(2));
-  }
-  return filepath;
+    if (filepath)
+        return filepath.replaceAll('~/', os.homedir());
+    else
+        return filepath
+}
+
+async function loadConfig(configName) {
+    const filePath = path.join(CONFIG_PATH, configName);
+
+    return new Promise((resolve, reject) => {
+        if (!configName.endsWith('.json')) return reject(400);
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) return reject(400)
+            return resolve(data)
+        });
+    })
+}
+
+async function countFiles(configName) {
+    return new Promise((resolve, reject) => {
+        try {
+            const data = loadConfig(configName);
+            let fileCount = 0;
+            data.forEach(element => {
+                if (element.file_path) {
+                    const fullPath = expandHome(element.file_path);
+                    fs.stat(fullPath, (statErr, stats) => {
+                        if (statErr) {
+                            console.error(`Error getting stats for ${fullPath}: ${statErr.message}`);
+                        } else if (stats.isFile()) {
+                            fileCount++;
+                        }
+                    });
+                }
+            });
+            resolve(fileCount)
+        } catch (err) {
+            if (typeof err == 'number')
+                reject(err);
+            else
+                reject({ error: err });
+        }
+    })
 }
 
 const CONFIG_PATH = expandHome(process.env.CONFIG_PATH || './config');
 console.log(`config path: ${CONFIG_PATH}`)
 
+const UPLOAD_EXEC = expandHome(process.env.UPLOAD_EXEC || undefined)
+console.log(`upload exec: ${UPLOAD_EXEC}`)
+
 // List JSON files
-router.get('/files', (req, res) => {
+router.get('/configs', (req, res) => {
     fs.readdir(CONFIG_PATH, (err, files) => {
         if (err) return res.status(500).json({ error: 'Unable to read config directory.' });
         const jsonFiles = files.filter(file => file.endsWith('.json'));
@@ -26,29 +69,66 @@ router.get('/files', (req, res) => {
 });
 
 // Get JSON file content
-router.get('/file/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(CONFIG_PATH, filename);
-
-    if (!filename.endsWith('.json')) return res.status(400).json({ error: 'Invalid file type.' });
-
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) return res.status(500).json({ error: 'Unable to read file.' });
-        res.json(JSON.parse(data));
-    });
+router.get('/config/:configName', (req, res) => {
+    try {
+        const data = loadConfig(req.params.configName)
+        return res.json(JSON.parse(data))
+    } catch (err) {
+        return res.status(err).json({ error: err == 400 ? 'Invalid file type.' : 'Unable to read file.' });
+    }
 });
 
 // Save changes to JSON file
-router.post('/file/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(CONFIG_PATH, filename);
+router.post('/config/:configName', (req, res) => {
+    const configName = req.params.configName;
+    const filePath = path.join(CONFIG_PATH, configName);
 
-    if (!filename.endsWith('.json')) return res.status(400).json({ error: 'Invalid file type.' });
+    if (!configName.endsWith('.json')) return res.status(400).json({ error: 'Invalid file type.' });
 
     fs.writeFile(filePath, JSON.stringify(req.body, null, 2), err => {
         if (err) return res.status(500).json({ error: 'Unable to write file.' });
         res.json({ success: true });
     });
 });
+
+router.get('/content/pending-count/:configName', async (req, res) => {
+    try {
+        res.status(200).json({ pendingCount: await countFiles(req.params.configName) })
+    } catch (err) {
+        if (typeof err == 'number')
+            res.status(err).json({ error: err == 400 ? 'Invalid file type.' : 'Unable to read file.' });
+        else
+            res.status(500).json({ error: 'failed to count files', err });
+    }
+})
+
+router.post('/content/upload/:configName', (req, res) => {
+    if (!UPLOAD_EXEC) {
+        return res.status(500).json({ error: 'upload command is not configured' })
+    }
+
+    const configName = req.params.configName
+    const profilePath = expandHome('~/.bashrc');
+    const command = `source ${profilePath} && ${UPLOAD_EXEC}`;
+
+    exec(command, async (error, stdout, stderr) => {
+        if (error) {
+            const errMsg = `exec error: ${error}`;
+            console.error(errMsg);
+            res.status(500).json({ error: errMsg })
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+
+        try {
+            const filesLeft = await countFiles(configName)
+            res.status(200).json({ pendingCount: filesLeft })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ error: 'failed to count files left', err })
+        }
+    });
+})
 
 module.exports = router
